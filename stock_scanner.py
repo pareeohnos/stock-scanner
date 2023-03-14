17f40ea2fa93b6a5e2ee1cb0c203710449de4f63
Stockxavier <126023143+Stockxavier@users.noreply.github.com>
2023-03-14 14:54:49 +0100

    change

---
+++ stock_scanner.py
@@ -0,0 +1,18 @@
+
+#STOCK SCANNER
+
+#importing libs 
+import pandas as pd 
+import numpy as np
+import requests
+import datetime
+
+
+#api key ( from FMP free tier)
+
+fmp_api_key = '8008db2cb50d4f4ac05a20f7d283a232'
+
+
+
+#getting list of stocks
+
